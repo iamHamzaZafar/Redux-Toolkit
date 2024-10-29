28238d03addb697e2b483ce23cdd00e4a203c10b
Hamza Zafar <link2hamzazafar@gmail.com>
2024-10-29 21:05:57 +0500

    used redux hokks to intract with the store

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,28 @@
 import { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux';
 import './App.css'
+import { decrement, reset, incrementByAmount , incremet } from './features/counter/counterSlice';
 
 function App() {
   const[amount , setAmount] = useState(0);
+  const count = useSelector(state => state.counter.value)
+  const dispatch = useDispatch() ;
 
   return (
     <>
-      <button className='btn'>+</button>
-      <h3 className='txt'>Count:</h3>
-      <button className='btn '>-</button>
+    <h1>This is the Basic Project which is Created using Redux Tool Kit </h1>
+      <button onClick={()=>dispatch(incremet())} className='btn'>+</button>
+      <h3 className='txt'>Count:{count}</h3>
+      <button  onClick={()=>dispatch(decrement())} className='btn '>-</button>
       <br />
       <br />
-      <button className='btn'>Reset</button>
+      <button  onClick={()=>dispatch(reset())} className='btn'>Reset</button>
       <br />
       <br />
       <input className='input' onChange={(e)=>setAmount(e.target.value)} value={amount} placeholder='Enter a number' type="number" />
       <br />
       <br />
-      <button className='btn'>Inc By Amount</button>
+      <button  onClick={()=>dispatch(incrementByAmount(amount))}   className='btn'>Inc By Amount</button>
     </>
   )
 }
--- src/features/counter/counterSlice.js
+++ src/features/counter/counterSlice.js
@@ -8,15 +8,18 @@ export const counterSlice = createSlice({
         incremet: state =>{
             state.value+=1 ;
         },
+        reset:state=>{
+            state.value= 0 
+        },
         decrement: state=>{
             state.value-= 1;
         },
         incrementByAmount:(state,action)=>{
-            state.value+= action.payload ;
-        }
+            state.value+= Number( action.payload );
+        },
 
     }
 })
 
-export const {incremet, decrement , incrementByAmount} = counterSlice.actions 
+export const {incremet, decrement , reset , incrementByAmount} = counterSlice.actions 
 export default counterSlice.reducer
