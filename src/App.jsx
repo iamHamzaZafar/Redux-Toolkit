1dc11584bdfa0214ef978df666d83852ad483f17
Hamza Zafar <link2hamzazafar@gmail.com>
2024-10-29 20:34:17 +0500

    completed the ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,23 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const[amount , setAmount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button className='btn'>+</button>
+      <h3 className='txt'>Count:</h3>
+      <button className='btn '>-</button>
+      <br />
+      <br />
+      <button className='btn'>Reset</button>
+      <br />
+      <br />
+      <input className='input' onChange={(e)=>setAmount(e.target.value)} value={amount} placeholder='Enter a number' type="number" />
+      <br />
+      <br />
+      <button className='btn'>Inc By Amount</button>
     </>
   )
 }
